3c3a080d89a46968154fff3e47245ee4a41cad28
Mairan Teodoro <mteodoro@stsci.edu>
2025-08-19 14:41:40 -0400

    Fix issue with romanisim installing wrong RDM version.

--- scripts/simulate_and_process_l1_files.sh
+++ scripts/simulate_and_process_l1_files.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# requires that PYTHONPATH and PATH contain the roman_photoz/scripts directory.
-
 # stop on error
 set -e
 
@@ -25,19 +23,19 @@ python -m update_romanisim_catalog_fluxes \
   --output-filename romanisim_input_catalog_fluxes_updated.ecsv
 
 # 4 - run romanisim-make-image on the catalog from step 3
-run_romanisim.sh ${filter_list}
+./run_romanisim.sh ${filter_list}
 
 # 5 - create association files for ELP and run roman_elp
 find *_uncal.asdf | xargs -I{} -P8 -n1 strun roman_elp {}
 
 # 6 - create the association files for skycells
-create_skycell_asn.sh ${filter_list}
+./create_skycell_asn.sh ${filter_list}
 
 # 7 - create association files for MOS and run roman_mos
 find r00001_p*asn.json | xargs -I{} -P8 -n1 strun roman_mos {}
 
 # 8 - create association files for multiband catalog
-create_asn_for_mbandcatalog.sh *_coadd.asdf
+./create_asn_for_mbandcatalog.sh *_coadd.asdf
 
 # 9 - run MultibandCatalogStep
 find mbcat_*_wfi01.json | xargs -I{} -P8 -n1 strun romancal.step.MultibandCatalogStep {} --snr_threshold 5
